26345684803168c2af2010c4c566caa37ff0ea41
SonHoang118 <son1182004@gmail.com>
2026-08-13 21:08:06 +0700

    update name and logo app

---
+++ app/manifest.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "DHS To do",
+    short_name: "DHS To do",
+    description: "DHS To do",
+    start_url: "/",
+    display: "standalone",
+    icons: [
+      {
+        src: "/logoApp.png",
+        sizes: "any",
+        type: "image/png",
+      },
+    ],
+  };
+}
